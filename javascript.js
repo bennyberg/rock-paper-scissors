85dca682169ee523cc4c466af4e5bd524f63822a
bennyberg <73042035+bennyberg@users.noreply.github.com>
2025-09-23 17:05:14 +0300

    added complete game functionality

--- javascript.js
+++ javascript.js
@@ -1,26 +1,95 @@
 const ROCK = "rock";
 const PAPER = "paper";
 const SCISSORS = "scissors";
+const ROUNDS = 5;
 
 let humanScore = 0;
 let computerScore = 0;
 
-const getComputerChoice = function(){
+const getComputerChoice = function () {
     const choice = Math.random();
 
-    if (choice < 0.33){
+    if (choice < 0.33) {
         return ROCK;
     }
-    else if (choice < 0.66){
+    else if (choice < 0.66) {
         return PAPER
     }
-    else{
+    else {
         return SCISSORS;
-    }  
+    }
 }
 
-const getHumanChoice = function(){
-    userChoice = prompt();
+const getHumanChoice = function () {
+    let humanChoice = prompt("Rock, paper or scissors?");
 
-    return userChoice;
+    return humanChoice;
 }
+
+const playRound = function (humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!")
+    }
+    else if (humanChoice === PAPER) {
+        switch (computerChoice) {
+            case ROCK:
+                console.log("You win! Paper beats rock");
+                humanScore++;
+                break;
+            case SCISSORS:
+                console.log("You lose! Scissors beat paper");
+                computerScore++;
+                break;
+        }
+    }
+    else if (humanChoice === ROCK) {
+        switch (computerChoice) {
+            case PAPER:
+                console.log("You lose! Paper beats rock");
+                computerScore++;
+                break;
+            case SCISSORS:
+                console.log("You win! Rock beats scissors");
+                humanScore++;
+
+                break;
+        }
+    }
+    else if (humanChoice === SCISSORS) {
+        switch (computerChoice) {
+            case PAPER:
+                console.log("You win! Scissors beat paper");
+                humanScore++;
+                break;
+            case ROCK:
+                console.log("You lose! Rock beats scissors");
+                computerScore++;
+                break;
+        }
+    }
+}
+
+const playGame = function () {
+    let humanSelection = getHumanChoice();
+    console.log(humanSelection);
+    let computerSelection = getComputerChoice();
+    console.log(computerSelection);
+    playRound(humanSelection, computerSelection);
+
+    for (let i = 1; i < ROUNDS; i++) {
+        humanSelection = getHumanChoice();
+        console.log(humanSelection);
+
+        computerSelection = getComputerChoice()
+        console.log(computerSelection);
+;
+        playRound(humanSelection, computerSelection);
+    }
+}
+
+playGame();
+
+
+
+
